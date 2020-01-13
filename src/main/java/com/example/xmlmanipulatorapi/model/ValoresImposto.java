ed254e683093c31e102572be2dedcfe63739652e
André Lagacione <lagacioneandre@gmail.com>
2020-01-13 09:57:38 -0300

    add anotation for xml element

--- src/main/java/com/example/xmlmanipulatorapi/model/ValoresImposto.java
+++ src/main/java/com/example/xmlmanipulatorapi/model/ValoresImposto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Getter
@@ -15,11 +16,17 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "valoresImposto")
 public class ValoresImposto {
 
+    @XmlElement(name = "cst")
     private String cst;
+    @XmlElement(name = "vBC")
     private Double vBC;
+    @XmlElement(name = "pIcms")
     private Double pIcms;
+    @XmlElement(name = "vIcms")
     private Double vIcms;
+    @XmlElement(name = "cfop")
     private Double cfop;
+    @XmlElement(name = "pCprb")
     private Double pCprb;
 
 }
--- src/main/java/com/example/xmlmanipulatorapi/model/XmlNfeZip.java
+++ src/main/java/com/example/xmlmanipulatorapi/model/XmlNfeZip.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Getter
@@ -15,9 +16,13 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "xmlNfeZip")
 public class XmlNfeZip {
 
+    @XmlElement(name = "chaveAcesso")
     private String chaveAcesso;
+    @XmlElement(name = "serieNFe")
     private String serieNFe;
+    @XmlElement(name = "numeroNFe")
     private String numeroNFe;
+    @XmlElement(name = "valorFrete")
     private String valorFrete;
 
 }
