8fe4204fc9d00bb6d2a0c4471e7e8e5f96905e01
andreLagacione <lagacioneandre@gmail.com>
2020-01-13 22:27:38 -0300

    create mongo configuration

--- src/main/java/com/example/xmlmanipulatorapi/MongoConfiguration.java
+++ src/main/java/com/example/xmlmanipulatorapi/MongoConfiguration.java
@@ -1,4 +1,15 @@
 package com.example.xmlmanipulatorapi;
 
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MongoConfiguration {
+
+    @Bean
+    public MongoClient mongo() {
+        return new MongoClient("localhost");
+    }
+
 }
