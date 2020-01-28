0425c5daae04503f8cb8dfb5ae565011ce95ebd1
andreLagacione <lagacioneandre@gmail.com>
2020-01-27 22:57:34 -0300

    map node

--- src/main/java/com/example/xmlmanipulatorapi/xpath/service/XpathService.java
+++ src/main/java/com/example/xmlmanipulatorapi/xpath/service/XpathService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -15,8 +16,6 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @Service
@@ -30,17 +29,44 @@ public class XpathService {
     }
 
     public void processarDocumento(MultipartFile file) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
-        File convertedFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getName());
-        file.transferTo(convertedFile);
-        FileInputStream fileInputStream = new FileInputStream(convertedFile);
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        Document document = documentBuilder.parse(fileInputStream);
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        String expression = "cteProc";
-        NodeList nodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
+        String fileName = file.getOriginalFilename();
 
-        System.out.println(nodeList);
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document xml = db.parse(fileName);
+
+        XPathFactory xpf = XPathFactory.newInstance();
+        XPath xPath = xpf.newXPath();
+
+        Node nodes = (Node) xPath.evaluate("/cteProc", xml, XPathConstants.NODE);
+
+//        for (int i = 0; i < nodes.getLength(); i++) {
+//            System.out.println("Node name: " + nodes.item(i).getNodeName() + " - " + "Node value: " + nodes.item(i).getNodeValue());
+//        }
+
+        NodeList childs = nodes.getChildNodes();
+
+        for (int n = childs.getLength() - 1; n >= 0; n--) {
+            Node child = childs.item(n);
+            short nodeType = child.getNodeType();
+            if (nodeType == Node.ELEMENT_NODE) {
+                System.out.println("Element node: " + child);
+            }
+            else if (nodeType == Node.TEXT_NODE) {
+                String trimmedNodeVal = child.getNodeValue().trim();
+                if (trimmedNodeVal.length() == 0){
+                    System.out.println("Node vazio");
+//                    nodes.removeChild(child);
+                }
+                else {
+                    System.out.println("Node value: " + trimmedNodeVal);
+                    child.setNodeValue(trimmedNodeVal);
+                }
+            } else if (nodeType == Node.COMMENT_NODE) {
+                nodes.removeChild(child);
+            }
+        }
+
     }
 
 }
