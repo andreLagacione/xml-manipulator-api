30be8f2ecd57e00cbce8fba9b15477ed91a9b3a1
andreLagacione <lagacioneandre@gmail.com>
2020-01-29 21:16:11 -0300

    change type

--- src/main/java/com/example/xmlmanipulatorapi/editarDocumento/entity/EditarDocumento.java
+++ src/main/java/com/example/xmlmanipulatorapi/editarDocumento/entity/EditarDocumento.java
@@ -6,15 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.io.File;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "xpathDocument")
+@Document(collection = "editedDocument")
 public class EditarDocumento {
 
-    private File file;
+    private String editedDocument;
 
 }
