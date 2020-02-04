78d255709fc678be744f5508de24a96f4948341e
André Lagacione <lagacioneandre@gmail.com>
2020-02-04 14:02:09 -0300

    add prop for id

--- src/main/java/com/example/xmlmanipulatorapi/document/model/DocumentXml.java
+++ src/main/java/com/example/xmlmanipulatorapi/document/model/DocumentXml.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -13,6 +14,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "document")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DocumentXml {
+
+    @Id
+    @JsonProperty
+    private String id;
 
     @JsonProperty("cteProc")
     private CteProc cteProc;
